43708abfbfab4e5c7003627e8ed5445bce7bb019
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2025-08-20 11:48:40 +0100

    Capturing render errors

--- src/utils/render.cpp
+++ src/utils/render.cpp
@@ -1,9 +1,16 @@
 #include "render.hpp"
 
+#include <exception>
 #include <inja/inja.hpp>
 #include <string>
 
+#include "exceptions/exceptions.hpp"
+
 std::string libtokamap::render(const std::string& template_string, const inja::json& data)
 {
-    return inja::render(inja::render(template_string, data), data);
+    try {
+        return inja::render(inja::render(template_string, data), data);
+    } catch (const std::exception& e) {
+        throw libtokamap::MappingError(e.what());
+    }
 }
